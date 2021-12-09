a30ca8c620d95f01c9fff7576cc9e9195baaa76a
jeeveshlodhi <lodhijeevesh2000@gmail.com>
2021-12-09 12:18:48 +0530

    v1

---
+++ mylib/config.py
@@ -0,0 +1,9 @@
+MAIL = ''
+url = ''
+ALERT = False
+Threshold = 10
+Thread = True
+Log = False
+Scheduler = False
+Timer = False
+#python run.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input videos/v.mp4
